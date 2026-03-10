d6401dc71ba852df2fc0e7e91372135df452ad7d
vidyarthikabir@gmail.com <vidyartk@submit-a.ib.coehpc>
2026-03-09 17:39:47 -0700

    changed saved model name formatting ot not include bad characters

--- final_project.py
+++ final_project.py
@@ -18,7 +18,7 @@ from meta_config import *
 #work for sunday - split this into a train function, an evaluate function, and a load data function
 def main():
     # Hyperparameters
-    if USES_HPC: # hpc doesn't like having batch sizes above 16
+    if USES_HPC: # hpc doesn't like having batch sizes above 8
         batch_size = 8
     else:
         batch_size = 16
@@ -26,7 +26,7 @@ def main():
         lr = 1e-4
     else:
         lr = 3e-4
-    num_epochs = 20
+    num_epochs = 15
 
     # Dataset + Dataloaders
     dataset = IAMLinesDataset(transform=transform)
@@ -49,12 +49,10 @@ def main():
                              num_workers=0, collate_fn=collate_fn)
     testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
                             num_workers=0, collate_fn=collate_fn)
-
+    
     num_batches = len(trainloader)
     datapoints_per_epoch = 10 # doesn't actually impact wandb recording, only meant to determine how many (persistient) printouts we have between epochs
-    epoch_interval = num_batches // datapoints_per_epoch
-
-    print(f"{epoch_interval} batches per data entry")
+    epoch_interval = (num_batches // datapoints_per_epoch) + 1 # lazy round up so there's never an excess record
 
     # Model
     model = CRNN(num_classes=len(characters)+1).to(device)
@@ -68,6 +66,7 @@ def main():
             factor=0.5,
             patience=2
         )
+
     print("loaded model")
 
     if WANDB_RECORDING:
@@ -105,12 +104,12 @@ def main():
             train_loss_total += loss.item()
 
 
-            if (i + 1) % epoch_interval == 0:
+            if (i + 1) % epoch_interval == 0 or i + 1 == num_batches:
                 interval_train_loss = (train_loss_total - prev_total) / epoch_interval
-                print(f'Epoch [{epoch+1}/{num_epochs}] Batch [{i+1}/{num_batches}] Train Loss: {interval_train_loss:.4f} Elapsed Time: {runtime}')
+                print(f'Epoch [{epoch+1:02}/{num_epochs}] Batch [{i+1:04}/{num_batches}] Train Loss: {interval_train_loss:.3f} Elapsed Time: {runtime}')
                 prev_total = train_loss_total
             else:
-                print(f'Epoch [{epoch+1}/{num_epochs}] Batch [{i+1}/{num_batches}] Train Loss: {loss.item():.4f} Elapsed Time: {runtime}', end='\r')
+                print(f'Epoch [{epoch+1:02}/{num_epochs}] Batch [{i+1:04}/{num_batches}] Train Loss: {loss.item():.3f} Elapsed Time: {runtime}', end='\r')
 
         epoch_train_loss = train_loss_total / num_batches
 
@@ -118,6 +117,7 @@ def main():
         model.eval()
         val_loss_total = 0
         all_preds, all_labels = [], []
+        logged_images = None
         with torch.no_grad():
             for images, labels in testloader:
                 images = images.to(device)
@@ -128,27 +128,41 @@ def main():
                 input_lengths = torch.full((outputs.size(0),),outputs.size(1),dtype=torch.long).to(device)
                 loss = criterion(outputs.permute(1,0,2), targets, input_lengths, target_lengths)
                 val_loss_total += loss.item()
-                all_preds.extend(ctc_greedy_decode(outputs))
+                preds = ctc_greedy_decode(outputs)
+                all_preds.extend(preds)
                 all_labels.extend(labels)
+
+                if WANDB_RECORDING and logged_images is None:
+                    logged_images = []
+
+                    for j in range(min(5,images.size(0))):
+                        caption = f"GT: {labels[j]} | Pred: {preds[j]}"
+                        logged_images.append(wandb.Image(images[j].cpu(), caption=caption))
+
         epoch_val_loss = val_loss_total / len(testloader)
         cer = compute_cer(all_preds, all_labels)
+        
         if SCHEDULAR:
             scheduler.step(epoch_val_loss)
-        if epoch % 1 == 0:
+
+        if not WANDB_RECORDING:
             for i in range(3):
                 print("GT :", all_labels[i])
                 print("PR :", all_preds[i])
+
         print("Unique preds:", torch.unique(torch.argmax(outputs,2)))
         print(f"Epoch [{epoch+1}/{num_epochs}] Train Loss: {epoch_train_loss:.4f} | Val Loss: {epoch_val_loss:.4f} | CER: {cer:.4f}")
+
         if WANDB_RECORDING:
-            wandb.log({"train_loss": epoch_train_loss, "val_loss": epoch_val_loss, "CER": cer})
+            wandb.log({"train_loss": epoch_train_loss, "val_loss": epoch_val_loss, "CER": cer, "validation_examples": logged_images})
     
     #cleanup
     if WANDB_RECORDING:
         wandb.finish()
 
     if SAVE_MODEL:
-        save_path = f"{dir_path}/models/{MODEL_FILENAME}_{start_time}.pth"
+        timestamp = start_time.strftime("%Y-%m-%d_%H-%M-%S")
+        save_path = f"{dir_path}/models/{MODEL_FILENAME}_{timestamp}.pth"
 
         torch.save(model.state_dict(), save_path)
 
